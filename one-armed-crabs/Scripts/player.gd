391588cad4d3104857142a2428b082be4350a4a2
Rui <157858530+Ruilesser@users.noreply.github.com>
2025-02-15 14:30:36 -0700

    Added player movement code

---
+++ one-armed-crabs/Scripts/player.gd
@@ -0,0 +1,48 @@
+extends CharacterBody2D
+
+@export var isPlayer2 = false
+
+@export var SPEED = 120.0
+@export var JUMP_VELOCITY = -300.0
+
+@export_range(0, 1) var deceleration = 0.1
+@export_range(0, 1) var acceleration = 0.1
+@export_range(0, 1) var decelerate_on_jump_release = 0.5
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	
+	if isPlayer2 == false:
+		# Handle jump.
+		if Input.is_action_just_pressed("p1_up") and is_on_floor():
+			velocity.y = JUMP_VELOCITY
+			
+		if Input.is_action_just_released("p1_up") and velocity.y < 0:
+			velocity.y *= decelerate_on_jump_release
+
+		# Get the input direction and handle the movement/deceleration.
+		# As good practice, you should replace UI actions with custom gameplay actions.
+		var direction := Input.get_axis("p1_left", "p1_right")
+		if direction:
+			velocity.x = move_toward(velocity.x, direction * SPEED, SPEED * acceleration)
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED * deceleration)
+	else:
+		# Handle jump.
+		if Input.is_action_just_pressed("p2_up") and is_on_floor():
+			velocity.y = JUMP_VELOCITY
+			
+		if Input.is_action_just_released("p2_up") and velocity.y < 0:
+			velocity.y *= decelerate_on_jump_release
+
+		# Get the input direction and handle the movement/deceleration.
+		# As good practice, you should replace UI actions with custom gameplay actions.
+		var direction := Input.get_axis("p2_left", "p2_right")
+		if direction:
+			velocity.x = move_toward(velocity.x, direction * SPEED, SPEED * acceleration)
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED * deceleration)
+	
+	move_and_slide()
