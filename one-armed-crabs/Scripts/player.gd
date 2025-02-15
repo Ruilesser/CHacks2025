df621b2e99b1f59b8ec7215ebbfabcbade3b7be8
Rui <157858530+Ruilesser@users.noreply.github.com>
2025-02-15 15:12:23 -0700

    Player 1 movement changes

--- one-armed-crabs/Scripts/player.gd
+++ one-armed-crabs/Scripts/player.gd
@@ -2,12 +2,14 @@ extends CharacterBody2D
 
 @export var isPlayer2 = false
 
-@export var SPEED = 120.0
-@export var JUMP_VELOCITY = -300.0
+@export var SPEED = 160.0
+@export var JUMP_VELOCITY = -250.0
+@export var horizontal_jump_velocity = 80
 
-@export_range(0, 1) var deceleration = 0.1
+@export_range(0, 1) var deceleration = 0.5
 @export_range(0, 1) var acceleration = 0.1
-@export_range(0, 1) var decelerate_on_jump_release = 0.5
+@export_range(0, 1) var decelerate_on_jump_release = 0.2
+
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -26,23 +28,12 @@ func _physics_process(delta: float) -> void:
 		# As good practice, you should replace UI actions with custom gameplay actions.
 		var direction := Input.get_axis("p1_left", "p1_right")
 		if direction:
-			velocity.x = move_toward(velocity.x, direction * SPEED, SPEED * acceleration)
+			if is_on_floor():
+				velocity.x = move_toward(velocity.x, direction * SPEED, SPEED * acceleration)
+			else:
+				velocity.x = move_toward(velocity.x, direction * (SPEED + horizontal_jump_velocity), SPEED * acceleration)
 		else:
 			velocity.x = move_toward(velocity.x, 0, SPEED * deceleration)
-	else:
-		# Handle jump.
-		if Input.is_action_just_pressed("p2_up") and is_on_floor():
-			velocity.y = JUMP_VELOCITY
-			
-		if Input.is_action_just_released("p2_up") and velocity.y < 0:
-			velocity.y *= decelerate_on_jump_release
 
-		# Get the input direction and handle the movement/deceleration.
-		# As good practice, you should replace UI actions with custom gameplay actions.
-		var direction := Input.get_axis("p2_left", "p2_right")
-		if direction:
-			velocity.x = move_toward(velocity.x, direction * SPEED, SPEED * acceleration)
-		else:
-			velocity.x = move_toward(velocity.x, 0, SPEED * deceleration)
 	
 	move_and_slide()
